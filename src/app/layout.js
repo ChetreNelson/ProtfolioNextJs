8e09baf7ce6bccecd6fc5cd043fdf86a87ebc088
ChetreNelson <128913141+ChetreNelson@users.noreply.github.com>
2024-05-22 14:59:30 +0545

    removing unneccsary imports

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
+import TransitionProvider from "@/components/transitionProvider";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from "@/components/navbar";
-import TransitionProvider from "@/components/transitionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
