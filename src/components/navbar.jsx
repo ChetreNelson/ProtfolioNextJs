0a5b1ac1664d91338b6bf4fd7f490f9a79ff1d60
ChetreNelson <128913141+ChetreNelson@users.noreply.github.com>
2024-05-22 15:02:04 +0545

    testing push

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useState } from "react";
 import NavLink from "./navLink";
 import { motion } from "framer-motion";
+
 const links = [
   { url: "/", title: "Home" },
   { url: "/about", title: "About" },
