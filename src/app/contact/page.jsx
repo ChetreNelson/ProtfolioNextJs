33194642ea6373c6cc529200ef9180da4c9100e2
ChetreNelson <128913141+ChetreNelson@users.noreply.github.com>
2024-05-26 08:18:29 +0545

    adding emailjs for sending email functionality

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,35 @@
-"use client"
-import { motion } from "framer-motion"
+"use client";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
+
 const ContactPage = () => {
+  const [sucess, setSuccess] = useState(false);
+  const [err, setErr] = useState(false);
+  const text = "Say Hello";
+  const formRef = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        NEXT_PUBLIC_TEMPLATE_ID,
+        form.current,
+        {
+          publicKey: "YOUR_PUBLIC_KEY",
+        }
+      )
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
   return (
     <motion.div
       className="h-full"
@@ -8,7 +37,57 @@ const ContactPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      Contact
+      <div className="h-full flex flex-col lg:flex-row  px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+        {/* text-Containter  */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex justify-center items-center text-6xl">
+          <div>
+            {text.split("").map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{
+                  duration: 3,
+                  repeat: Infinity,
+                  delay: index * 0.1,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+            😊
+          </div>
+        </div>
+        {/* form-containter */}
+        <form ref={formRef} className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24 ">
+          <span>Dear Nelson,</span>
+          <textarea
+            rows={6}
+            className="bg-transparent border-b-2 border-b-black outline-none resize-none"
+            name="user_message"
+          />
+          <span>My mail address is:</span>
+          <input
+            name="user_email"
+            type="text"
+            className="bg-transparent border-b-2 border-b-black outline-none "
+          />
+          <span>Regards</span>
+          <button className="bg-purple-200 rounded font-semibold text-gray-600 p-4">
+            Send
+          </button>
+          {sucess && (
+            <span className="text-green-600 font-semibold">
+              Your message has been sent sucessfully!
+            </span>
+          )}
+          {err && (
+            <span className="text-red-600 font-semibold">
+              Something went wrong!
+            </span>
+          )}
+        </form>
+      </div>
     </motion.div>
   );
 };
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -5,10 +5,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRef } from "react";
 
-const ProtfolioPage = () => {
+const PortfolioPage = () => {
   const ref = useRef();
+
   const { scrollYProgress } = useScroll({ target: ref });
-  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-100%"]);
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-80%"]);
 
   return (
     <motion.div
@@ -17,11 +18,11 @@ const ProtfolioPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="h-[600vh]" ref={ref}>
+      <div className="h-[600vh] relative" ref={ref}>
         <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
           My Works
         </div>
-        <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden ">
+        <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
           <motion.div style={{ x }} className="flex">
             <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300" />
             {Projects.map((item) => (
@@ -30,17 +31,19 @@ const ProtfolioPage = () => {
                 key={item.id}
               >
                 <div className="flex flex-col gap-8 text-white">
-                  <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl ">
+                  <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl">
                     {item.title}
                   </h1>
                   <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px]">
                     <Image src={item.img} alt="" fill />
                   </div>
-                  <p className="w-80 md:w-96 lg:w-[500px] lg:text-lg xl:w-[600px] ">
+                  <p className="w-80 md:w96 lg:w-[500px] lg:text-lg xl:w-[600px]">
                     {item.desc}
                   </p>
-                  <Link href={item.link} className=" flex justify-end">
-                    <button className="p-2 text-sm md:p-4 md:text-md lg:p-8 lg:tex-lg bg-white text-gray-600 font-semibold  m-4 rounded">See Demo</button>
+                  <Link href={item.link} className="flex justify-end">
+                    <button className="p-2 text-sm md:p-4 md:text-md lg:p-8 lg:text-lg bg-white text-gray-600 font-semibold m-4 rounded">
+                      See Demo
+                    </button>
                   </Link>
                 </div>
               </div>
@@ -49,7 +52,7 @@ const ProtfolioPage = () => {
         </div>
       </div>
       <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
-        <h1 className="text-8xl">Do you have a project? </h1>
+        <h1 className="text-8xl">Do you have a project?</h1>
         <div className="relative">
           <motion.svg
             animate={{ rotate: 360 }}
@@ -60,12 +63,12 @@ const ProtfolioPage = () => {
             <defs>
               <path
                 id="circlePath"
-                d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60 ,60 0 0,1 -120,0"
+                d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
               />
             </defs>
             <text fill="#000">
               <textPath xlinkHref="#circlePath" className="text-xl">
-                Front-end Developer
+                Front-end Developer 
               </textPath>
             </text>
           </motion.svg>
@@ -81,4 +84,4 @@ const ProtfolioPage = () => {
   );
 };
 
-export default ProtfolioPage;
+export default PortfolioPage;
