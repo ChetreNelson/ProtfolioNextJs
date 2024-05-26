c3cd0a4e49900c7e69ba49f75111ce186c5ab475
ChetreNelson <128913141+ChetreNelson@users.noreply.github.com>
2024-05-26 07:36:48 +0545

    working on protfolio page:

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -30,18 +30,51 @@ const ProtfolioPage = () => {
                 key={item.id}
               >
                 <div className="flex flex-col gap-8 text-white">
-                  <h1>{item.title}</h1>
-                  <div className="relative">
+                  <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl ">
+                    {item.title}
+                  </h1>
+                  <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px]">
                     <Image src={item.img} alt="" fill />
                   </div>
-                  <p>{item.desc}</p>
-                  <Link href={item.link}>
-                    <button>See Demo</button>
+                  <p className="w-80 md:w-96 lg:w-[500px] lg:text-lg xl:w-[600px] ">
+                    {item.desc}
+                  </p>
+                  <Link href={item.link} className=" flex justify-end">
+                    <button className="p-2 text-sm md:p-4 md:text-md lg:p-8 lg:tex-lg bg-white text-gray-600 font-semibold  m-4 rounded">See Demo</button>
                   </Link>
                 </div>
               </div>
             ))}
           </motion.div>
+        </div>
+      </div>
+      <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
+        <h1 className="text-8xl">Do you have a project? </h1>
+        <div className="relative">
+          <motion.svg
+            animate={{ rotate: 360 }}
+            transition={{ duration: 8, ease: "linear", repeat: Infinity }}
+            viewBox="0 0 300 300"
+            className="w-64 h-64 md:w-[500px] md:h-[500px] "
+          >
+            <defs>
+              <path
+                id="circlePath"
+                d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60 ,60 0 0,1 -120,0"
+              />
+            </defs>
+            <text fill="#000">
+              <textPath xlinkHref="#circlePath" className="text-xl">
+                Front-end Developer
+              </textPath>
+            </text>
+          </motion.svg>
+          <Link
+            href="/contact"
+            className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center"
+          >
+            Hire Me
+          </Link>
         </div>
       </div>
     </motion.div>
