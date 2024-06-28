87c7b53003b0718f437c55c95a6d1f3370fe896c
ChetreNelson <128913141+ChetreNelson@users.noreply.github.com>
2024-06-28 13:26:02 +0545

    refacotoring the profile p

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,7 +15,7 @@ const Homepage = () => {
       <div className=" h-full flex flex-col lg:flex-row-reverse justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 ">
         {/* IMAGE CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 relative ">
-          <Image src="/hero.png" alt="" fill className="object-contain " />
+          <Image src="/heroOne.png" alt="" fill className="object-contain " />
         </div>
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
